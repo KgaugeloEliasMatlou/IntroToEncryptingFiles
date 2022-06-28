0bb9e8cf9ee90ee90c2afe7e8c4a19cb71aed016
KgaugeloEliasMatlou <eliasmatlou66@gmail.com>
2022-06-28 11:27:42 +0200

    Add project files.

---
+++ IntroToEncryptingFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class program
+{
+    //public static void EncryptAFile(string x)
+    //{
+    //    File.Encrypt(x);
+
+    //}
+    //public static void DecryptAFile(string x)
+    //{
+    //    File.Decrypt(x);
+
+    //}
+
+
+    public static void ReadFile()
+    {
+        int counter = 0;
+        string line;
+
+        // read the file and display it line by line.
+        System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Train 08\Desktop\C#.txt");
+        while ((line = file.ReadLine()) != null)
+            Console.WriteLine(line);
+        counter++;
+
+        file.Close();
+    }
+    static void Main()
+    {
+        ReadFile();
+
+
+    }
+}
